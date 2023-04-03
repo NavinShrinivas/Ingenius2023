eb03340e22a449a3befe1a5757f4129d95b63b82
Saksham-13 <sakshamalok13@gmail.com>
2023-04-03 12:22:20 +0530

    Fix: Removed extra space on rigth

--- ingenius23/src/components/hero.js
+++ ingenius23/src/components/hero.js
@@ -5,16 +5,16 @@ export  const Banner = () => {
     return (
      <>
     <div
-    className=' relative text-center min-h-screen '
+    className='   text-center min-h-screen '
     >
     <Parallax speed={-15}>
-      <div className="absolute top-0 w-full slow">
+      <div className="    w-full slow">
         <Image
             src="https://media.discordapp.net/attachments/972456002844766228/1091712713010462801/Main_Logo.png?width=1036&height=1036"
             alt="Picture of the author"
             width={500}
             height={500}
-            className='mx-auto'
+            className=' mx-auto'
             />
 
       </div>
@@ -22,7 +22,7 @@ export  const Banner = () => {
     <Parallax speed={15}>
 
       <div
-        className=" text-center w-full  absolute top-72  my-40 text-[#fab518] heading text-5xl font-lovelo-line-light lg:text-9xl font-bold "
+        className=" text-center  w-full  text-[#fab518] heading text-5xl font-lovelo-line-light lg:text-9xl font-bold "
         >
           <h1
           className=''
