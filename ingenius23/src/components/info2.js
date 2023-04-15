aa2739c9b8e9fd0361082e77653d022da8915d78
ArnavKumar7 <arnav4776@gmail.com>
2023-04-15 21:19:32 +0530

    add:registration link, open innovation track, title and css for each

--- ingenius23/src/components/info2.js
+++ ingenius23/src/components/info2.js
@@ -1,38 +1,36 @@
-'use client';
-import Image from 'next/image';
-import { Parallax } from 'react-scroll-parallax';
-import AOS from 'aos';
-import 'aos/dist/aos.css';
-import { useEffect } from 'react';
-export  const Info2 = () => {
-    useEffect(() => {
-        AOS.init();
-        }, []);
-    return(
-        <div
-        data-aos="fade-up"
-        data-aos-delay="200"
-        className='items-center my-20 gap-10  lg:px-40 justify-center flex'
-        >
+import Image from "next/image";
+import { Parallax } from "react-scroll-parallax";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from "react";
+export const Info2 = () => {
+  useEffect(() => {
+    AOS.init();
+  }, []);
+  return (
+    <div
+      data-aos="fade-up"
+      data-aos-delay="200"
+      className="items-center my-20 gap-10  lg:px-40 justify-center flex"
+    >
+      <div className=" font-mono">
+        <h1 className="text-4xl  py-4 text-[#fab518] font-bold">Open Source</h1>
+        <p className="text-xl">
+          Students have access to public repositories on GitHub and will be
+          judged on their contributions to improve open source software and
+          thereby are encouraged to foster the spirit of FOSS which allows one
+          to use and modify software collaboratively.
+        </p>
+      </div>
 
-                <div
-                className=' font-mono'
-                >
-                    <h1 className='text-4xl  py-4 text-[#fab518] font-bold'>Open Source</h1>
-                    <p className='text-xl'>Students have access to public repositories on GitHub and will be judged on their contributions to improve open source software and thereby are encouraged to foster the spirit of FOSS which allows one to use and modify software collaboratively.</p>
-                </div>
-
-
-            <div className="lg:flex hidden items-center  w-full  ">
-                <Image
-                src="/team.svg"
-                alt="Picture of the author"
-                width={500}
-                height={500}
-                />
-                
-               
-            </div>
-        </div>
-    )
-}
+      <div className="lg:flex hidden items-center  w-full  ">
+        <Image
+          src="/team.svg"
+          alt="Picture of the author"
+          width={500}
+          height={500}
+        />
+      </div>
+    </div>
+  );
+};
--- ingenius23/src/components/sponsors.js
+++ ingenius23/src/components/sponsors.js
@@ -1,60 +1,57 @@
 import Image from "next/image";
 export const Sponsor = () => {
-    return (
-        <>
-        <div>
-            <div className="flex flex-col items-center justify-center">
-                <h1 className="text-6xl font-bold text-[#fab518]">Sponsors</h1>
-            </div>
-            {/* a div for grid of images */}
-            <div className="  px-10 my-10  ">
-                <div className=" grid grid-cols-1  lg:grid-cols-4  ">
-                <Image
-                src="https://media.discordapp.net/attachments/972456002844766228/1095888869720858664/wolfram-language-text-logo.png?width=1420&height=502"
-                alt="Picture of the author"
-                width={200}
-                height={200}
-                className="mx-auto my-6"
-                 data-aos="fade-right"
-                  data-aos-delay="200"
-                  data-aos-duration="600"
-                />
-                 <Image
-                src="/team.svg"
-                alt="Picture of the author"
-                width={200}
-                height={200}
-                className="mx-auto my-6"
-                 data-aos="fade-right"
-                  data-aos-delay="200"
-                  data-aos-duration="600"
-                />
-                 <Image
-                src="/team.svg"
-                alt="Picture of the author"
-                width={200}
-                height={200}
-                className="mx-auto my-6"
-                 data-aos="fade-right"
-                  data-aos-delay="200"
-                  data-aos-duration="600"
-                />
-                 <Image
-                src="/team.svg"
-                alt="Picture of the author"
-                width={200}
-                height={200}
-                className="mx-auto my-6"
-                 data-aos="fade-right"
-                  data-aos-delay="200"
-                  data-aos-duration="600"
-                />
-                </div>
-                </div>
-
+  return (
+    <>
+      <div>
+        <div className="flex flex-col items-center justify-center lg:mt-5">
+          <h1 className="text-6xl font-bold text-[#fab518]">Sponsors</h1>
         </div>
-            
-            
-        </>
-    );
-    }
+        {/* a div for grid of images */}
+        <div className="  px-10 my-10  ">
+          <div className=" flex justify-center  ">
+            <Image
+              src="https://media.discordapp.net/attachments/972456002844766228/1095888869720858664/wolfram-language-text-logo.png?width=1420&height=502"
+              alt="Picture of the author"
+              width={200}
+              height={200}
+              className="mx-auto my-6"
+              data-aos="fade-right"
+              data-aos-delay="200"
+              data-aos-duration="600"
+            />
+            {/* <Image
+                src="/team.svg"
+                alt="Picture of the author"
+                width={200}
+                height={200}
+                className="mx-auto my-6"
+                 data-aos="fade-right"
+                  data-aos-delay="200"
+                  data-aos-duration="600"
+                />
+                 <Image
+                src="/team.svg"
+                alt="Picture of the author"
+                width={200}
+                height={200}
+                className="mx-auto my-6"
+                 data-aos="fade-right"
+                  data-aos-delay="200"
+                  data-aos-duration="600"
+                />
+                 <Image
+                src="/team.svg"
+                alt="Picture of the author"
+                width={200}
+                height={200}
+                className="mx-auto my-6"
+                 data-aos="fade-right"
+                  data-aos-delay="200"
+                  data-aos-duration="600"
+                /> */}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
