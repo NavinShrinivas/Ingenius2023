6f72700a863bed2fd0a29b15553344b1ba313019
ArnavKumar7 <arnav4776@gmail.com>
2023-04-22 15:03:05 +0530

    fix: mobile screen logo placement

--- ingenius23/src/components/sponsors.js
+++ ingenius23/src/components/sponsors.js
@@ -8,17 +8,7 @@ export const Sponsor = () => {
         </div>
         {/* a div for grid of images */}
         <div className="  px-10 my-10  ">
-          <div className=" flex justify-center  ">
-            <Image
-              src="https://media.discordapp.net/attachments/972456002844766228/1095888869720858664/wolfram-language-text-logo.png?width=1420&height=502"
-              alt="Picture of the author"
-              width={200}
-              height={200}
-              className="mx-auto my-6"
-              data-aos="fade-right"
-              data-aos-delay="200"
-              data-aos-duration="600"
-            />
+          <div className=" flex justify-center xs:flex-col">
             <Image
               src="/JuliaHub-Logo-color-black.svg"
               alt="Picture of the author"
@@ -29,6 +19,17 @@ export const Sponsor = () => {
               data-aos-delay="200"
               data-aos-duration="600"
             />
+            <Image
+              src="https://media.discordapp.net/attachments/972456002844766228/1095888869720858664/wolfram-language-text-logo.png?width=1420&height=502"
+              alt="Picture of the author"
+              width={200}
+              height={200}
+              className="mx-auto my-6"
+              data-aos="fade-right"
+              data-aos-delay="200"
+              data-aos-duration="600"
+            />
+
             {/* 
                  <Image
                 src="/team.svg"
