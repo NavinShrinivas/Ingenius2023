57d056e4651bcecaf7f381180b753423984ee2ca
anuragrao04 <raoanu2004@gmail.com>
2024-04-09 12:43:59 +0530

    adds: dark reader tag, themes, idea generator

--- ingenius23/src/components/hero.js
+++ ingenius23/src/components/hero.js
@@ -17,7 +17,7 @@ export const Banner = () => {
             />
             {/* a button */}
             <div className="flex flex-col items-center justify-center">
-              <button class="relative text-lg inline-flex items-center justify-center px-7 py-4 mb-2 mr-2 overflow-hidden font-medium text-zinc-900 border-solid border-4 border-[#fab518] rounded-xl bg-[#fac518] hover:bg-gradient-to-r hover:from-amber-500 hover:to-yellow-400 hover:text-white hover:text-bold hover:scale-110 hover:shadow-[0_0_45px_-10px_(0,0,0,0.3)] hover:shadow-cyan-500 transition duration-300 ease-in-out transform hover:-translate-y-1">
+              <button className="relative text-lg inline-flex items-center justify-center px-7 py-4 mb-2 mr-2 overflow-hidden font-medium text-zinc-900 border-solid border-4 border-[#fab518] rounded-xl bg-[#fac518] hover:bg-gradient-to-r hover:from-amber-500 hover:to-yellow-400 hover:text-white hover:text-bold hover:scale-110 hover:shadow-[0_0_45px_-10px_(0,0,0,0.3)] hover:shadow-cyan-500 transition duration-300 ease-in-out transform hover:-translate-y-1">
                 <Link href="https://forms.gle/d8heL1vE3cFGaKLD8">
                   Register Now
                 </Link>
@@ -25,9 +25,8 @@ export const Banner = () => {
             </div>
           </div>
         </Parallax>
-       <Parallax speed={15}>
-          <div  className=" absolute lg:-top-[520px] -top-[400px] z-10  text-center  w-[94%] lg:w-full  text-[#fab518] flex justify-center ">
-             
+        <Parallax speed={15}>
+          <div className=" absolute lg:-top-[520px] -top-[400px] z-10  text-center  w-[94%] lg:w-full  text-[#fab518] flex justify-center ">
             <h1 className="heading text-6xl font-lovelo-line-bold lg:font-lovelo-line-light lg:text-9xl font-bold flex flex-wrap justify-center">
               <div className="lg:hidden">&nbsp;</div>
               <div className="text-orange-600">I</div>
@@ -44,7 +43,6 @@ export const Banner = () => {
               <div className="text-indigo-600">2</div>
               <div className="text-pink-600">4</div>
             </h1>
-            
           </div>
         </Parallax>
       </div>
