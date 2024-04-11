1f45cc2fd506d644af92c9f1ac011788842b5c37
Achyuth Yogesh Sosale <113189939+achyuthcodes30@users.noreply.github.com>
2024-04-11 11:51:06 +0530

    Update info1.js

--- ingenius23/src/components/info1.js
+++ ingenius23/src/components/info1.js
@@ -27,10 +27,10 @@ export const Info1 = () => {
           <h1 className="lg:text-5xl text-3xl  py-4 text-[#fac518] font-bold">PESU-ECC&#39;s flagship hackathon is back!</h1>
           <p className="text-lg lg:text-xl text-white">
             
-Enter the realm of unparalleled innovation and visionary engineering at Ingenius 12.0, an electrifying 24-hour hackathon set to captivate minds and redefine boundaries on the 19th and 20th of April 2024.
+Enter the realm of unparalleled innovation and visionary engineering at InGenius 12.0, an electrifying 24-hour hackathon set to captivate minds and redefine boundaries on the 19th and 20th of April 2024.
           </p>
           <p className="text-lg lg:text-xl text-white mt-6">
-            Whether you&#39;re a coder, designer, or just curious, Ingenius provides the perfect platform for you to connect, collaborate, and create through the power of OPEN INNOVATION! &#128161;
+            Whether you&#39;re a coder, designer, or just curious, InGenius provides the perfect platform for you to connect, collaborate, and create through the power of OPEN INNOVATION! &#128161;
           </p>
         </div>
       </div>
