9bf76e9271893aa82eea2c4178eeb9acaf6a054f
anuragrao04 <raoanu2004@gmail.com>
2024-04-17 22:20:01 +0530

    fixed api url

--- Ingenius23_backend/main.go
+++ Ingenius23_backend/main.go
@@ -83,6 +83,57 @@ func main() {
 		}
 	})
 
+
+
+	// this method is not secure. It offers a way to get the token from only the SRN without veryifying other details of the user
+	// this is there as a backup if QR code reading fails
+	// the frontend route /volunteer will hit this method if the qr code reading fails and they enter SRN manually
+	// to make sure the participant is not cheating the system, the volunteer doing the checkins should verify the details of the participants and make sure they are who they say they are (check ID card or something)
+	r.POST("/getTokenFromSRN", func(c *gin.Context) {
+		var b communication.GetTokenFromSRNRequest
+		err := c.BindJSON(&b)
+		if err != nil {
+			log.Println(err)
+			c.JSON(http.StatusBadRequest, gin.H{
+				"status":  false,
+				"message": "invalid request",
+			})
+			return
+		}
+		status, fulluserrecord := database.CheckUserRecords_INSECURE(b)
+		if status {
+			message, httpstatus, status, token, _ := authentication.GenerateAuthToken(*fulluserrecord)
+			if status == false {
+				log.Println(err)
+				c.JSON(httpstatus, gin.H{
+					"status":  false,
+					"message": message,
+				})
+			} else {
+				if fulluserrecord.Checkin == true {
+					//Business logic whether check in only once.
+					c.JSON(http.StatusOK, gin.H{
+						"status":  true,
+						"message": "Already checked in!",
+						"token":      token,
+					})
+				} else {
+					database.SetCheckedInUser(*fulluserrecord)
+					c.JSON(http.StatusOK, gin.H{
+						"status":  true,
+						"message": "Checked in successfully!",
+						"token": token,
+					})
+				}
+			}
+		} else {
+			c.JSON(http.StatusForbidden, gin.H{
+				"status":  false,
+				"message": "Invalid User record!",
+			})
+		}
+	})
+
 	r.POST("/createuser", func(c *gin.Context) {
 		var b communication.UserInitRequest
 		err := c.BindJSON(&b)
